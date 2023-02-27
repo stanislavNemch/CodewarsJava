481a1a3238febb93e5f7f231b5df127a212111fb
Stanislav Nemchenko <stas.buka@gmail.com>
2023-02-27 03:49:08 +0200

    New Task Vowels

--- src/Vowels.java
+++ src/Vowels.java
@@ -1,2 +1,40 @@
 public class Vowels {
+
+    public static int getCount(String str) {
+
+        char[] strToArray = str.toCharArray();
+        int count = 0;
+
+        for (char c : strToArray) {
+            if ('a' == c || 'e' == c || 'i' == c ||
+                    'o' == c || 'u' == c) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"));
+    }
 }
+/*
+*
+* private static List<Character> vowels;
+
+  static {
+    vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
+  }
+
+  public static int getCount(String str) {
+    int vowelsCount = 0;
+    for(int i = 0; i < str.length(); i++) {
+      if (vowels.contains(str.charAt(i))) {
+        vowelsCount++;
+      }
+    }
+    return vowelsCount;
+  }
+*
+* */
